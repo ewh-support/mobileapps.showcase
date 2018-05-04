b1d28d34610e402db4486a0f60185f35c201f908
showcase <showcase@email.com>
2018-05-04 10:27:15 +0700

    04/05/2018 10:27:12.86

---
+++ js/app.js
@@ -0,0 +1,135 @@
+// Dom7
+var $$ = Dom7;
+
+// Framework7 App main instance
+var app  = new Framework7({
+  root: '#app', // App root element
+  id: 'io.framework7.testapp', // App bundle ID
+  name: 'Framework7', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+      // Demo products for Catalog section
+      products: [
+        {
+          id: '1',
+          title: 'Apple iPhone 8',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+        },
+        {
+          id: '2',
+          title: 'Apple iPhone 8 Plus',
+          description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+        },
+        {
+          id: '3',
+          title: 'Apple iPhone X',
+          description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+        },
+      ]
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+});
+
+// Init/Create views
+var homeView = app.views.create('#view-home', {
+  url: '/'
+});
+var catalogView = app.views.create('#view-catalog', {
+  url: '/catalog/'
+});
+var settingsView = app.views.create('#view-settings', {
+  url: '/settings/'
+});
+
+//easyweb: login thành công, thiết lập accesstoken vào header
+var apibackend = 'http://103.199.18.44:2990/api';
+axios.defaults.baseURL = apibackend;
+axios.defaults.headers.common['Authorization'] = localStorage.accessToken;
+
+app.on('pageInit', function (page) {
+  // localStorage.isAuthenticated = 'false'  ; //để debug
+  if (localStorage.isAuthenticated == 'true')  {
+    //đã đăng nhập
+  }else {
+    //chưa đăng nhập: có thể hiện ra login
+    app.loginScreen.open('#my-login-screen');
+  }
+});
+
+
+// Login Screen Demo
+$$('#my-login-screen .login-button').on('click', function () {
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  //easyweb
+  login();
+
+  // Alert username and password
+  //app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
+
+//Easyweb
+function login(){
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  //easyweb
+  var credentials = { username: username, password: password }; //đặt tên là credentials: thông tin bảo mật
+
+  axios.post('/Users/login', credentials)
+  .then(function (response) {
+    console.log(response.data)
+    if (response.data)  {
+      var data = response.data; //dữ liệu trả về bởi loopback3
+      localStorage.isAuthenticated = 'true' ; //là string, ko phải bool
+      localStorage.accessToken = data.id;
+      localStorage.userId = data.userId;
+      localStorage.ttl = data.ttl; //ttl: time to lease, thời gian mà accesstoken hết hạn
+
+      axios.defaults.headers.common['Authorization'] = localStorage.accessToken;
+
+      getUserInfo();
+      // Close login screen
+      app.loginScreen.close('#my-login-screen');
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+function getUserInfo() {
+  console.log('getUserInfo()')
+  if (localStorage.isAuthenticated !== 'true')  {
+    //lấy thông tin đầy đủ của user
+    axios.get('/Users/' + localStorage.userId)
+    .then(function(response){
+      console.log(response.data)
+      var data = response.data;
+      localStorage.username = data.username;
+      localStorage.email = data.email;
+      localStorage.isAuthenticated = 'true'
+    })
+    .catch(function(error) {
+      localStorage.userId = '';
+      localStorage.isAuthenticated = 'false';
+    })
+    return true;
+  }else {
+    return false;
+  }
+}
---
+++ js/routes.js
@@ -0,0 +1,165 @@
+routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+  {
+    path: '/catalog/',
+    componentUrl: './pages/catalog.html',
+  },
+  {
+    path: '/product/:id/',
+    componentUrl: './pages/product.html',
+  },
+  {
+    path: '/settings/',
+    url: './pages/settings.html',
+  },
+  // Page Loaders & Router
+  {
+    path: '/page-loader-template7/:user/:userId/:posts/:postId/',
+    templateUrl: './pages/page-loader-template7.html',
+  },
+  {
+    path: '/page-loader-component/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/page-loader-component.html',
+  },
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = routeTo.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            componentUrl: './pages/request-and-load.html',
+          },
+          {
+            context: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },
+  //easyweb
+  {
+    path: '/profiles',
+    on: {  //tham khảo https://framework7.io/docs/routes.html#route-events
+        pageAfterIn: function (e, page) {
+          // do something after page gets into the view
+        },
+        pageInit: function (e, page) {
+          console.log('do something when page initialized')   // do something when page initialized
+          $$('.convert-form-to-data').on('click', function(){
+            var formData = app.form.convertToData('#my-profile');
+
+            //easyweb gọi api để cập nhật dữ liệu, lưu ở formData
+            console.log(JSON.stringify(formData));
+            axios.patch('/Users/' + formData.id, formData)
+            .then(function(response) {
+              console.log('cập nhật thành công')
+            })
+            .catch(function(error) {
+
+            })
+          });
+
+          $$('.fill-form-from-data').on('click', function(){
+            var oldData = app.form.convertToData('#my-profile');
+            console.log('object id', oldData.id);
+
+            var formData = {} ; //axios.get()
+            app.form.fillFromData('#my-form', formData);
+          });
+        },
+      }
+    ,
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+      // App instance
+      var app = router.app;
+      //var axios = app.axios;
+      if (localStorage.isAuthenticated !== 'true') {
+        app.dialog.alert('Hãy đăng nhập để truy cập thông tin', 'Thông Báo');
+        //chưa đăng nhập: có thể hiện ra login
+        app.loginScreen.open('#my-login-screen');
+        return;
+      }
+
+      // Show Preloader
+      app.preloader.show();
+
+      console.log('getUserInfo()')
+        //lấy thông tin đầy đủ của user
+      axios.get('/Users/' + localStorage.userId)
+        .then(function(response){
+          console.log(response.data)
+          var data = response.data;
+          data.isAuthenticated = true;
+
+          var formId = 'my-profile';
+          app.form.storeFormData(formId, data) //
+          // Resolve route to load page
+          resolve(
+            {
+              componentUrl: './pages/profiles.html',
+            },
+            {
+              context: {
+                username: data.username, //chỉ cần truyền thông tin ngoài form
+              }
+            }
+
+          );
+        })
+        .catch(function(error) {
+        })
+        // Hide Preloader
+        app.preloader.hide();
+        return true;
+    },
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
