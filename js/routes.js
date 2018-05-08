0043361d9f3d160662f85d97c6cb1f9e8c112738
showcase <showcase@email.com>
2018-05-08 17:27:51 +0700

    05/08/2018 17:27:48.03

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,25 @@
-routes = [
-  {
+routes = [{
     path: '/',
     url: './index.html',
   },
   {
     path: '/about/',
-    url: './pages/about.html',
+    url: './pages/about.html'
+  },
+  {
+    path: '/map/',
+    url: './pages/map.html',
+    on: {
+      pageInit: function (e, page) {
+        var map = new google.maps.Map(document.getElementById('gmap'), {
+          center: {
+            lat: -34.397,
+            lng: 150.644
+          },
+          zoom: 8
+        });
+      }
+    }
   },
   {
     path: '/catalog/',
@@ -50,8 +64,7 @@ routes = [
           firstName: 'Vladimir',
           lastName: 'Kharlampidi',
           about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
+          links: [{
               title: 'Framework7 Website',
               url: 'http://framework7.io',
             },
@@ -65,52 +78,48 @@ routes = [
         app.preloader.hide();
 
         // Resolve route to load page
-        resolve(
-          {
-            componentUrl: './pages/request-and-load.html',
-          },
-          {
-            context: {
-              user: user,
-            }
+        resolve({
+          componentUrl: './pages/request-and-load.html',
+        }, {
+          context: {
+            user: user,
           }
-        );
+        });
       }, 1000);
     },
   },
   //easyweb
   {
     path: '/profiles',
-    on: {  //tham khảo https://framework7.io/docs/routes.html#route-events
-        pageAfterIn: function (e, page) {
-          // do something after page gets into the view
-        },
-        pageInit: function (e, page) {
-          console.log('do something when page initialized')   // do something when page initialized
-          $$('.convert-form-to-data').on('click', function(){
-            var formData = app.form.convertToData('#my-profile');
+    on: { //tham khảo https://framework7.io/docs/routes.html#route-events
+      pageAfterIn: function (e, page) {
+        // do something after page gets into the view
+      },
+      pageInit: function (e, page) {
+        console.log('do something when page initialized') // do something when page initialized
+        $$('.convert-form-to-data').on('click', function () {
+          var formData = app.form.convertToData('#my-profile');
 
-            //easyweb gọi api để cập nhật dữ liệu, lưu ở formData
-            console.log(JSON.stringify(formData));
-            axios.patch('/Users/' + formData.id, formData)
-            .then(function(response) {
+          //easyweb gọi api để cập nhật dữ liệu, lưu ở formData
+          console.log(JSON.stringify(formData));
+          axios.patch('/Users/' + formData.id, formData)
+            .then(function (response) {
               console.log('cập nhật thành công')
             })
-            .catch(function(error) {
+            .catch(function (error) {
 
             })
-          });
+        });
 
-          $$('.fill-form-from-data').on('click', function(){
-            var oldData = app.form.convertToData('#my-profile');
-            console.log('object id', oldData.id);
+        $$('.fill-form-from-data').on('click', function () {
+          var oldData = app.form.convertToData('#my-profile');
+          console.log('object id', oldData.id);
 
-            var formData = {} ; //axios.get()
-            app.form.fillFromData('#my-form', formData);
-          });
-        },
-      }
-    ,
+          var formData = {}; //axios.get()
+          app.form.fillFromData('#my-form', formData);
+        });
+      },
+    },
     async: function (routeTo, routeFrom, resolve, reject) {
       // Router instance
       var router = this;
@@ -128,9 +137,9 @@ routes = [
       app.preloader.show();
 
       console.log('getUserInfo()')
-        //lấy thông tin đầy đủ của user
+      //lấy thông tin đầy đủ của user
       axios.get('/Users/' + localStorage.userId)
-        .then(function(response){
+        .then(function (response) {
           console.log(response.data)
           var data = response.data;
           data.isAuthenticated = true;
@@ -138,11 +147,9 @@ routes = [
           var formId = 'my-profile';
           app.form.storeFormData(formId, data) //
           // Resolve route to load page
-          resolve(
-            {
+          resolve({
               componentUrl: './pages/profiles.html',
-            },
-            {
+            }, {
               context: {
                 username: data.username, //chỉ cần truyền thông tin ngoài form
               }
@@ -150,12 +157,84 @@ routes = [
 
           );
         })
-        .catch(function(error) {
-        })
-        // Hide Preloader
-        app.preloader.hide();
-        return true;
+        .catch(function (error) {})
+      // Hide Preloader
+      app.preloader.hide();
+      return true;
     },
+  },
+  {
+    path: '/news/',
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+      // App instance
+      var app = router.app;
+      // Show Preloader
+      app.preloader.show();
+
+      console.log('get news()')
+
+      axios.get('http://128.199.153.64:5000/api/BaiViets', {
+          params: {
+            filter: {
+              "trangThai": "PUBLISHED"
+            }
+          }
+        }).then(function (response) {
+          console.log(response.data)
+          var data = response.data;
+
+          // Resolve route to load page
+          resolve({
+            templateUrl: './pages/news.html',
+          }, {
+            context: {
+              data_news: data, //chỉ cần truyền thông tin ngoài form
+            }
+          });
+        })
+        .catch(function (error) {})
+      // Hide Preloader
+      app.preloader.hide();
+      return true;
+    }
+  },
+  {
+    /* news detail */
+    path: '/pages/news-detail/:id/',
+    templateUrl: './pages/news-detail.html',
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+      // App instance
+      var app = router.app;
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var id = routeTo.params.id;
+
+      console.log('news detail | id', id)
+
+      axios.get('http://128.199.153.64:5000/api/BaiViets/' + id).then(function (response) {
+          console.log(response.data)
+          var data = response.data;
+
+          // Resolve route to load page
+          resolve({
+            templateUrl: './pages/news-detail.html',
+          }, {
+            context: {
+              news_detail: data, //chỉ cần truyền thông tin ngoài form
+            }
+          });
+        })
+        .catch(function (error) {})
+      // Hide Preloader
+      app.preloader.hide();
+      return true;
+    }
   },
   // Default route (404 page). MUST BE THE LAST
   {
